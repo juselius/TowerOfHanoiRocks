cc265e9151bbd6ea5f90a278d2b9c17e1b54d9df
Jonas Juselius <jonas.juselius@itpartner.no>
2018-09-03 20:29:41 +0200

    Improve app, and add easter egg

--- src/HanoiRocks/App.fs
+++ src/HanoiRocks/App.fs
@@ -1,76 +1,40 @@
-namespace FsHanoi
+namespace HanoiRocks
 
 module Hanoi =
-    open Fable.Core
     open Fable.Helpers.React
     open Elmish
     open Elmish.React
-    open TowerOfHanoi
     open Fulma
-
-    type Model = {
-        nDisks : int    
-        nFizz : int    
-        }
-
-    type Msg =
-        | More
-        | Less
+    open View
 
     let init () : Model * Cmd<Msg> =
-        let model = { 
-            nDisks = 3 
-            nFizz = 20
-            }
-        model, Cmd.none
+        { nDisks = 3; nBuzz = 15; hello = "" }, Cmd.none
 
     let update (msg : Msg) (model : Model) : Model * Cmd<Msg> =
         match msg with
         | More -> 
-            let model' = {
-                nDisks = model.nDisks + 1
-                nFizz = model.nFizz + 5
-            }
+            let model' = 
+                { model with
+                    nDisks = model.nDisks + 1
+                    nBuzz = model.nBuzz + 5
+                }
             model', Cmd.none
         | Less -> 
-            let model' = {
-                nDisks = if model.nDisks > 0 then model.nDisks - 1 else 0
-                nFizz = if model.nFizz > 5 then model.nFizz - 5 else 0
-            }
+            let model' = 
+                { model with
+                    nDisks = if model.nDisks > 0 then model.nDisks - 1 else 0
+                    nBuzz = if model.nBuzz > 5 then model.nBuzz - 5 else 5
+                }
             model', Cmd.none
+        | Hello s -> {model with hello = s}, Cmd.none
 
     let view (model : Model) (dispatch : Msg -> unit) =
-        div [] [
-            Views.navbar
+        div [] [ 
+            View.navbar 
             Container.container [] [
-                Content.content [] [
-                    h2 [] [ str "Hello Hanoi!" ]
-                    ul [] ( 
-                        moveTower model.nDisks |> List.map (fun (n, f, t) -> 
-                            li [] [ 
-                                sprintf "Move %d from %d to %d" n f t |> str 
-                            ])
-                    )
-                    h2 [] [ str "Hello Fizzbuzz!" ]
-                    Box.box' [] [
-                        ul [] ( 
-                            fizzbuzz model.nFizz |> List.map (fun s -> 
-                                li [] [ str s ])
-                        )
-                    ]
-                ]
-                Button.button [ 
-                    Button.OnClick (fun _ -> dispatch More) 
-                    Button.Color IsPrimary
-                    ] [ str "More!"] 
-                str " "
-                Button.button [ 
-                    Button.OnClick (fun _ -> dispatch Less) 
-                    Button.Color IsWarning
-                    ] [ str "Less"] 
-                br []
+                hanoiBuzz model dispatch
             ]
-            Views.footer
+            View.footer
         ]
 
     #if DEBUG
--- src/HanoiRocks/View.fs
+++ src/HanoiRocks/View.fs
@@ -1,22 +1,89 @@
-namespace FsHanoi 
+namespace HanoiRocks
 
-module Views =
+module View =
     open Fable.Helpers.React
     open Fulma
 
-    let navbar = 
-        Navbar.navbar [ Navbar.Color Color.IsPrimary ] [ 
-            Navbar.Item.div [] [ Heading.h4 [ 
+
+    type Model = {
+        nDisks : int
+        nBuzz : int
+        hello : string
+    }
+
+    type Msg = 
+        | More 
+        | Less
+        | Hello of string
+
+    let navbar =
+        Navbar.navbar [ Navbar.Color Color.IsPrimary ] [
+            Navbar.Item.div [] [ Heading.h4 [
                  Heading.Modifiers [ Modifier.TextColor IsWhite ]
-            ] [ str "FizzBuz in Hanoi" ] ] 
+            ] [ str "FizzBuz with Hanoi Rocks" ] ]
         ]
 
     let footer =
-        Footer.footer [] [ 
-            Content.content [ 
-                Content.Modifiers [ 
-                    Modifier.TextAlignment 
-                        (Screen.All, TextAlignment.Centered) 
-                ] 
+        Footer.footer [] [
+            Content.content [
+                Content.Modifiers [
+                    Modifier.TextAlignment
+                        (Screen.All, TextAlignment.Centered)
+                ]
             ] [ h4 [] [ str "May the foo be with you!" ] ]
+        ]
+
+    let col4 = 
+        Column.column [
+            Column.Width (Screen.All, Column.Is4) 
         ] 
+
+    let orientalBeat =
+        function
+        | (s : string) when s.ToLower () = "oriental beat" -> 
+            Image.image [ Image.Is128x128 ] [ 
+                img [ Props.Src <| "Images/Hanoi-Rocks.jpg" ] 
+            ]
+        | x -> h4 [] [ str x ]
+        
+
+    let hanoiBuzz (model : Model) (dispatch : Msg -> unit) =
+        Content.content [] [
+        h2 [] [ str "What?" ]
+        Control.div [] [
+            Input.text [ 
+                Input.Placeholder "Echo"
+                Input.OnChange <| fun e -> dispatch <| Hello e.Value
+            ]
+            br []
+            orientalBeat model.hello
+        ]
+        h2 [] [ str "Tower of FizzBuzz" ] 
+        Button.button [
+            Button.Color IsPrimary
+            Button.OnClick (fun _ -> dispatch More)
+        ] [str "More!" ]
+        Button.button [
+            Button.Color IsWarning
+            Button.OnClick (fun _ -> dispatch Less)
+        ] [str "Less"]
+        br []
+        Box.box' [] [
+            Columns.columns [] [
+                col4 [
+                ul [] (
+                    FsHanoi.TowerOfHanoi.moveTower model.nDisks 
+                    |> List.map (fun (n, f, t) -> 
+                        li [] [ str <| sprintf "Move %d from %d to %d" n f t])
+                    )
+                ]
+                col4 [
+                    ul [] (
+                        FsHanoi.Fizz.fizzbuzz model.nBuzz 
+                        |> List.map (fun s -> li [] [ str s ])
+                    )
+                ] 
+            ]
+        ]
+        br []
+    ]
